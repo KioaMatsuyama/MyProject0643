45a09ccd653df7909c00d67637512ba5de44e0f0
KioaMatsuyama <105226577+KioaMatsuyama@users.noreply.github.com>
2025-09-08 09:13:01 +0800

    qx_redirect_video.js

--- qx_redirect_video.js
+++ qx_redirect_video.js
@@ -1,5 +1,6 @@
-const target = "https://raw.githubusercontent.com/KioaMatsuyama/MyProject0643/refs/heads/main/output2.mp4";
+const target = "https://cdn.jsdelivr.net/gh/KioaMatsuyama/MyProject0643@refs/heads/main/output2.mp4";
 $done({
   status: 307,
   headers: { "Location": target }
+
 });
